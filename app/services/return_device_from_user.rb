402f47bd8e1780a7221b0247ac12e51dc357115a
gothic459 <d.sek464@gmail.com>
2025-05-17 01:51:57 +0200

    implement ReturnDeviceFromUser

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -9,8 +9,14 @@ class ReturnDeviceFromUser
 
   def call
     device = Device.find_by(serial_number: @serial_number)
+    raise ReturnDeviceError::DeviceNeverAssigned unless device
 
-    assignment = DeviceAssignment.find_by!(user_id: @from_user, device_id: device.id)
+    assignment = device.device_assignments.find_by!(user_id: @from_user, device_id: device.id)
+
+    raise ReturnDeviceError::DeviceNeverAssigned unless assignment
+    raise ReturnDeviceError::Unauthorized unless assignment.user_id == @user.id
+    raise ReturnDeviceError::DeviceAlreadyReturned unless assignment.returned_at.nil?
+
     assignment.update!(
       returned_at: Time.current
     )
