853aa0bb5d1aa1f0f4004b7356c98cead478b4d4
gothic459 <d.sek464@gmail.com>
2025-05-16 20:58:13 +0200

    fix assign_to_self action, create device, assign device to user on service call

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -9,9 +9,23 @@ class AssignDeviceToUser
 
   def call
     raise RegistrationError::Unauthorized unless assign_to_self?
+
+    #create a new device if it doesnt exist
+    device = Device.where(serial_number: @serial_number).first_or_create!
+
+    assignment = DeviceAssignment.create(
+      user_id: @requesting_user.id,
+      device_id: device.id,
+      assigned_at: Time.now,
+      returned_at: nil
+    )
+
   end
+
   private
   def assign_to_self?
-    @current_user == @requesting_user
+    @requesting_user.id == @new_device_owner_id
   end
+  private
+
 end
